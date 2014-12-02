8bc08c796d538c35d5a85f21f39aae03119e0fef
Logan Buckley <loganmhb@users.noreply.github.com>
2014-12-02 12:51:15 -0300

    Comment debug=True - don't use in production

--- eatsafeweb.py
+++ eatsafeweb.py
@@ -10,10 +10,11 @@ def index(): return render_template('index.html', maps_key=config.MAPS_KEY)
 
 @app.route('/api/<target>')
 def apiAccess(target):
-    queryUrl = 'http://api.eatsafechicago.com/' + target + '?' + urllib.urlencode(request.args)
+    queryUrl = 'http://api.eatsafechicago.com/' + target + '?' \
+               + urllib.urlencode(request.args)
     req = urllib2.Request(queryUrl)
     response = urllib2.urlopen(req)
     return response.read()
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)  ### Turn this OFF in production.
